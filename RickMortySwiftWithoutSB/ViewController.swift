dd8fd033cad19e017bb0dcce958c0c8a9083223e
Erencan <erncncbk@gmail.com>
2022-06-06 13:58:21 +0100

    Initial Commit

---
+++ RickMortySwiftWithoutSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMortySwiftWithoutSB
+//
+//  Created by Erencan on 6.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
